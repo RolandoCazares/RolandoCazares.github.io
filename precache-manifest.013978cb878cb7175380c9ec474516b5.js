28c9f06c1dad7f92dca9570aeea861b9a9348e81
RolandoCazares <99943023+RolandoCazares@users.noreply.github.com>
2022-12-14 11:49:54 -0700

    Add files via upload

---
+++ precache-manifest.013978cb878cb7175380c9ec474516b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a9dddfaef8614aea634",
+    "url": "/static/css/main.32956570.chunk.css"
+  },
+  {
+    "revision": "2a9dddfaef8614aea634",
+    "url": "/static/js/main.2a9dddfa.chunk.js"
+  },
+  {
+    "revision": "e5f5180fc221536f0ef8",
+    "url": "/static/js/1.e5f5180f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "36a064b5d1189aa74719b2fe19758e4a",
+    "url": "/static/media/fondo.36a064b5.jpg"
+  },
+  {
+    "revision": "128fbeb4718964b32aea25e6072716c4",
+    "url": "/index.html"
+  }
+];
